2df8016476914d0fa0cd0b450b199010790e34a5
Sebastian Pfitzner <pfitzseb@gmail.com>
2021-12-10 18:11:56 +0100

    Update main.jl

--- bin/main.jl
+++ bin/main.jl
@@ -1,7 +1,8 @@
 ENV["STARTSERVER"] = true
 ENV["GENIE_ENV"] = "prod"
 ENV["EARLYBIND"] = true
-ENV["HOST"] = ENV["JH_APP_URL"]
+
+ENV["HOST"] = match(r"https?://(.*)$", ENV["JH_APP_URL"])[1]
 
 push!(LOAD_PATH, abspath(normpath(joinpath(@__DIR__, "../src/"))))
 include("../bootstrap.jl")
